d3e94d7db8349a9b4c7cde5cb62e04effed3d89b
Mitfort <110680320+Mitfort@users.noreply.github.com>
2022-09-30 15:49:53 +0200

    Add files via upload

---
+++ kamien norzyce paper/kamien norzyce paper/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace kamien_norzyce_paper
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(23, 79);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(168, 117);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(319, 74);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(169, 117);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(172, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Play Again";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.restart);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(92, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Paper";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(382, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Rock";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(186, 19);
+            this.label3.MinimumSize = new System.Drawing.Size(140, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 30);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Player 1 win!";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(382, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 21);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Player 2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(81, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 21);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Player 1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(209, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 37);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(280, 117);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 37);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 283);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Rock Paper Scissors";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ kamien norzyce paper/kamien norzyce paper/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kamien_norzyce_paper
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        string winner = "";
+        string[] arr = { "rock.jpg", "scissors.jpg", "paper.jpg" };
+        int p1wins = 0;
+        int p2wins = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void restart(object sender, EventArgs e)
+        {
+            int rand1 = random.Next(1, 3);
+            int rand2 = random.Next(1, 3);
+            switch (rand1)
+            {
+                case 1:
+                    label1.Text = "Rock";
+                    pictureBox1.Image = Image.FromFile("rock.jpg");
+                    break;
+                case 2:
+                    label1.Text = "Scissors";
+                    pictureBox1.Image = Image.FromFile("scissors.jpg");
+                    break;
+                case 3:
+                    label1.Text = "Paper";
+                    pictureBox1.Image = Image.FromFile("paper.jpg");
+                    break;
+                default:
+                    break;
+            }
+
+            switch (rand2)
+            {
+                case 1:
+                    label2.Text = "Rock";
+                    pictureBox2.Image = Image.FromFile("rock.jpg");
+                    break;
+                case 2:
+                    label2.Text = "Scissors";
+                    pictureBox2.Image = Image.FromFile("scissors.jpg");
+                    break;
+                case 3:
+                    label2.Text = "Paper";
+                    pictureBox2.Image = Image.FromFile("paper.jpg");
+                    break;
+                default:
+                    break;
+            }
+            label3.Text = Check(rand1, rand2);
+            label6.Text = p1wins.ToString();
+            label7.Text = p2wins.ToString();
+        }
+
+        private string Check(int p1,int p2)
+        {
+            if (arr[p1] == arr[p2])
+                winner = "Draw!";
+            else if (arr[p1] == "rock.jpg" && arr[p2] == "scissors.jpg" ||
+                arr[p1] == "scissors.jpg" && arr[p2] == "paper.jpg" ||
+                arr[p1] == "paper.jpg" && arr[p2] == "rock.jpg"
+                ) winner = "Player 1 won!";
+            else
+                winner = "Player 2 won!";
+
+            if(winner == "Player 1 won!")
+            {
+                p1wins++;
+            }
+            else if(winner == "Player 2 won!")
+            {
+                p2wins++;
+            }
+            return winner;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
